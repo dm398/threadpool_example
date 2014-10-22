3d29a84a784c894e756f313ef2ba66f0528e4216
dm398 <dm398@exeter.ac.uk>
2014-10-22 09:25:00 +0100

    added doc comments to MyTask

--- Job.java
+++ Job.java
@@ -1,30 +1,27 @@
+/*
+ * Class Job of threadpool_example 22/10/14
+ */
 public class Job implements Runnable {
-  private int jobNumber;
-  
-  Job(int jobNumber) {
-    this.jobNumber = jobNumber;
-  }
+    private int jobNumber;
 
+    Job(int jobNumber) {
+        this.jobNumber = jobNumber;
+    }
 
-  public void run(){
-    // Undertake required work, here we will emulate it by sleeping for a period
-    System.out.println("Job: " + jobNumber + " is being processed by thread: " + Thread.currentThread.getname());
+    public void run(){
+        // Undertake required work, here we will emulate it by sleeping for a period
+        System.out.println("Job: " + jobNumber + " is being processed by thread: " + Thread.currentThread().getName());
 
-  try {
-    Thread.sleep((int) (1000));
-  
-  }
-  
-  catch (InterruptedException e) {}
+        try {
+            Thread.sleep((int) (1000));
 
-  System.out.println("Job: " + jobNumber + " is ending in thread: " + Thread.currentThread().getName());
-  
-  }
+        }
 
+        catch (InterruptedException e) {}
 
+        System.out.println("Job: " + jobNumber + " is ending in thread: " + Thread.currentThread().getName());
 
-
-
+    }
 
 
 
